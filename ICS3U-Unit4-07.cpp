a38640f00524be122ce21d67572ed475617b1dd6
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-28 02:35:45 +0000

    done

---
+++ ICS3U-Unit4-07.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Emmaneln Fofeyin
+//
+// Created by: Emmanuel Fofeyin
+// Created on: Oct 2022
+// This program uses loops in if statements
+
+
+#include <iostream>
+
+int main() {
+
+    int counter;
+
+    // Process and Output
+    for (counter = 1000; counter <= 2000; counter++) {
+        if (counter % 5 == 4)
+            std::cout << counter << "\n";
+        else
+            std::cout << counter << " ";
+    }
+
+    std::cout << "" << std::endl;
+    std::cout << "\nDone." << std::endl;
+}
